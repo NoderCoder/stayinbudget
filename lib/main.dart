fde2093d07db6aa75161f51b3743e7978f420fc7
Mantra Sutariya <mantrasutaria007@gmail.com>
2021-12-19 13:59:34 -0500

    Sample code written

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,103 @@ import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/widgets/chart.dart';
 import 'package:flutter_complete_guide/widgets/new_transaction.dart';
 import 'package:flutter_complete_guide/widgets/transaction_list.dart';
-import 'package:intl/intl.dart';
 import 'models/transaction.dart';
 
-void main() => runApp(MyApp());
+// void main() {
+//   runApp(MyApp());
+// }
+
+//===================Hive test
+
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+const favoritesBox = 'favorite_books';
+
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox<String>(favoritesBox);
+  runApp(MyHiveApp());
+}
+
+const List<String> books = [
+  // book name, index
+  'Harry Potter', // 0
+  'To Kill a Mockingbird', // 1
+  'The Hunger Games', // 2
+  'The Giver', // 3
+  'Brave New World', // 4
+  'Unwind', // 5
+  'World War Z', // 6
+  'The Lord of the Rings', // etc...
+  'The Hobbit',
+  'Moby Dick',
+  'War and Peace',
+  'Crime and Punishment',
+  'The Adventures of Huckleberry Finn',
+  'Catch-22',
+  'The Sound and the Fury',
+  'The Grapes of Wrath',
+  'Heart of Darkness',
+];
+
+class MyHiveApp extends StatefulWidget {
+  @override
+  _MyHiveAppState createState() => _MyHiveAppState();
+}
+
+class _MyHiveAppState extends State<MyHiveApp> {
+  Box<String> favouriateBooksBox;
+
+  onFavoritePressed(int bookIndex) {
+    if (favouriateBooksBox.containsKey(bookIndex)) {
+      favouriateBooksBox.delete(bookIndex);
+      return;
+    }
+    favouriateBooksBox.put(bookIndex, books[bookIndex]);
+  }
+
+  Widget getIcon(int bookIndex) {
+    if (favouriateBooksBox.containsKey(bookIndex)) {
+      return Icon(
+        Icons.favorite,
+        color: Colors.red,
+      );
+    }
+    return Icon(Icons.favorite_border);
+  }
+
+  @override
+  void initState() {
+    favouriateBooksBox = Hive.box(
+        favoritesBox); //Hive.Box fetches the box and loads it in the memory
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hive Test"),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: favouriateBooksBox.listenable(),
+        builder: (context, Box<String> box, _) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(books[index]),
+                trailing: IconButton(
+                  onPressed: () => onFavoritePressed(index),
+                  icon: getIcon(index),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
