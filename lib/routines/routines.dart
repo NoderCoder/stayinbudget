f4c563d4fca4e2050bf302365eb1636c6b38c2cf
Mantra <Mantrasutaria007@mail.com>
2022-01-05 22:20:21 -0500

    Seperated list and planninng to add Hive

--- lib/routines/routines.dart
+++ lib/routines/routines.dart
@@ -27,7 +27,8 @@ class RoutinesPage extends StatefulWidget {
 }
 
 class _RoutinesPageState extends State<RoutinesPage> {
-  bool _checked = false;
+  final bool _checked = false;
+  List<Routine> singleDayRoutineList = dailyRoutines;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,9 +48,40 @@ class _RoutinesPageState extends State<RoutinesPage> {
                         fontSize: 12,
                       )),
               children: [
-                RoutineTile("Day 1 of 30", "12/27/2022", '3'),
                 Text(stoicQuotesList[1].quoteText),
-                ...dailyRoutines.map<Widget>((routine) {
+                ...singleDayRoutineList.map<Widget>((routine) {
+                  return CheckboxListTile(
+                      title: Text(
+                        routine.title,
+                        style: Theme.of(context).textTheme.bodyText1.copyWith(
+                            decoration: routine.checked
+                                ? TextDecoration.lineThrough
+                                : TextDecoration.none),
+                      ),
+                      value: routine.checked,
+                      onChanged: (bool value) {
+                        setState(() {
+                          routine.checked = value;
+                          routine.checkedDate = DateTime.now();
+                        });
+                      });
+                })
+              ]),
+
+          //Another tile to test
+          ExpansionTile(
+              title: Text(
+                " Day ${widget.streakDay} of ${widget.totalDays} ",
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              subtitle: Text("12/27/2021",
+                  style: Theme.of(context).textTheme.bodyText1.copyWith(
+                        fontWeight: FontWeight.w100,
+                        fontSize: 12,
+                      )),
+              children: [
+                Text(stoicQuotesList[1].quoteText),
+                ...singleDayRoutineList.map<Widget>((routine) {
                   return CheckboxListTile(
                       title: Text(
                         routine.title,
@@ -73,94 +105,4 @@ class _RoutinesPageState extends State<RoutinesPage> {
   }
 }
 
-class RoutineTile extends StatelessWidget {
-  final String transactionTitle;
-  final String transactionDate;
-  final String transactionAmount;
-
-  RoutineTile(
-      this.transactionTitle, this.transactionDate, this.transactionAmount);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
-        child: Container(
-          width: double.infinity,
-          child: Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        backgroundColor:
-                            Theme.of(context).scaffoldBackgroundColor,
-                        foregroundColor: Colors.grey[700],
-                        child: const Icon(
-                          Icons.import_export_rounded,
-                          size: 32,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 16,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transactionTitle,
-                            style: Theme.of(context).textTheme.bodyText1,
-                          ),
-                          Text(transactionDate,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  .copyWith(
-                                    fontWeight: FontWeight.w100,
-                                    fontSize: 12,
-                                  )),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        transactionAmount,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.amber[300],
-                            borderRadius: BorderRadius.all(Radius.circular(4))),
-                        child: Padding(
-                          padding: const EdgeInsets.all(2.0),
-                          child: Text("+3%",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  .copyWith(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 12,
-                                  )),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+// Snapshot of dailyroutine list is stored for each day
