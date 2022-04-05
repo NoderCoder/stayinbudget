a3f797d470cdc3364bbb4fdf77c7d89c03663219
Mantra <Mantrasutaria007@mail.com>
2022-04-04 23:05:27 -0400

    commit before pull

--- lib/routines/routineCircles.dart
+++ lib/routines/routineCircles.dart
@@ -1,8 +1,4 @@
-//Updating to see the changes in the remote
-//Test 1 : Making chanes and pushnin gonline
-
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class StreakCircles extends StatefulWidget {
   static String id = "StreakCircles";
@@ -21,11 +17,6 @@ class _StreakCirclesState extends State<StreakCircles> {
       return Colors.amber;
     } else
       return Colors.white;
-  }
-
-//On clicking the circle
-  actionCompleted() {
-    setState(() {});
   }
 
   Color streakColor = Colors.amber;
@@ -88,7 +79,6 @@ class _StreakCirclesState extends State<StreakCircles> {
 
 class TappableCircle extends StatefulWidget {
   TappableCircle({Key key}) : super(key: key);
-
   @override
   _TappableCircleState createState() => _TappableCircleState();
 }
@@ -96,143 +86,9 @@ class TappableCircle extends StatefulWidget {
 class _TappableCircleState extends State<TappableCircle> {
   @override
   Widget build(BuildContext context) {
-    double cRadius = 15;
     return CircleAvatar(
-      radius: cRadius,
-      backgroundColor: Colors.grey,
-      child: CircleAvatar(
-        backgroundColor: Colors.white,
-        radius: cRadius - 2,
-      ),
+      backgroundColor: Colors.black12,
+      radius: 20,
     );
-  }
-}
-
-class StreakTable extends StatelessWidget {
-  const StreakTable({Key key}) : super(key: key);
-  static String id = "StreakTable";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Streakz"),
-      ),
-      body: Column(
-        children: [
-          CustomPaint(
-            painter: DrawDottedhorizontalline(),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    Center(
-                      child: Text(
-                        "09",
-                        style: GoogleFonts.allertaStencil(
-                            textStyle: TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold)),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              StreakBoxHolder(),
-              StreakBoxHolder(),
-              StreakBoxHolder(),
-              StreakBoxHolder(),
-              StreakBoxHolder(),
-              StreakBoxHolder(),
-            ],
-          ),
-          CustomPaint(
-            painter: DrawDottedhorizontalline(),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class StreakBoxHolder extends StatelessWidget {
-  const StreakBoxHolder({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CustomPaint(
-          painter: DrawHorizontalline(),
-        ),
-        SizedBox(
-          height: 52,
-          width: 52,
-          child: Center(
-            key: UniqueKey(),
-            //replace with button
-            child: TappableCircle(),
-          ),
-        ),
-        CustomPaint(
-          painter: DrawHorizontalline(),
-        ),
-      ],
-    );
-  }
-}
-
-/// PAinter
-/// class DrawDottedhorizontalline extends CustomPaint
-class DrawDottedhorizontalline extends CustomPainter {
-  Paint _paint;
-  DrawDottedhorizontalline() {
-    _paint = Paint();
-    _paint.color = Colors.black; //dots color
-    _paint.strokeWidth = 1; //dots thickness
-    _paint.strokeCap = StrokeCap.square; //dots corner edges
-  }
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    for (double i = -300; i < 300; i = i + 15) {
-      // 15 is space between dots
-      if (i % 3 == 0)
-        canvas.drawLine(Offset(i, 0.0), Offset(i + 1, 0.0), _paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
-  }
-}
-
-//Horizonatla line painter
-class DrawHorizontalline extends CustomPainter {
-  Paint _paint;
-  DrawHorizontalline() {
-    _paint = Paint();
-    _paint.color = Colors.black; //dots color
-    _paint.strokeWidth = 2; //dots thickness
-    _paint.strokeCap = StrokeCap.square; //dots corner edges
-  }
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    for (double i = -26; i < 26; i = i + 1) {
-      // 15 is space between dots
-      if (i % 3 == 0) canvas.drawLine(Offset(0, i), Offset(0.0, i + 1), _paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
   }
 }
